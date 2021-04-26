b20db57f02e1aee8c7fcd20b4a2628428f2ae66f
moshegplay <moshegplay@gmail.com>
2021-04-26 22:24:05 +0300

    Update functions_A.py

---
+++ lessons_2/functions_A.py
@@ -0,0 +1,38 @@
+#from random import randint
+#from time import sleep
+'''
+def Market():
+    print("now will calculate your marketing:\nprices:\n------------\ntomato=3 NIS\ncucamber=2 NIS\ncola=5 NIS\nchicken=20 NIS\n ")
+    ntomatos=int(input("enter how many ntomatos?\n"))
+    ncucambers=int(input("enter how many ncucambers?\n"))
+    colas=int(input("enter how many colas?\n"))
+    nchickens=int(input("enter how many nchickens?\n"))
+
+    print("\nsummary of your order:\n----------\ntomatos: " +str(ntomatos) + "\ncucambers: " +str(ncucambers) + "\ncolas: " +str(colas) + "\nchickens: " + str(nchickens))
+
+
+    summary=(ntomatos*3)+(ncucambers*2)+(colas*5)+(nchickens*20)
+    print("\nyou have to pay: " + str(summary) + " NIS whitout tax")
+    print("\nyou have to pay: " + str("%.2f" % (summary*1.17)) + " NIS whit tax")
+
+for i in range(10):
+    Market()
+'''
+
+'''
+def calculating():
+    num1=int(input("Enter a number: "))
+    num2=int(input("Enter a number: "))
+    print("Your new number is: " + str(num1*num2))
+
+calculating()
+'''
+
+'''
+def calculating2(x,y):
+    print("Your first number: " + str(x) + "\nYour second number: " + str(y))
+    print("\nYour new number is: " + str(x**y))
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+calculating2(a,b)
+'''
