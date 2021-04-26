6c32d4a663d6cd8a77f97da0f44a815d19e212a1
moshegplay <moshegplay@gmail.com>
2021-04-26 22:24:08 +0300

    Create functions_B.py

---
+++ lessons_2/functions_B.py
@@ -0,0 +1,42 @@
+#from random import randint
+#from time import sleep
+'''
+def calculating():
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter a number: "))
+    sum=num1*num2
+    print("Your new number is: " + str(sum))
+    return sum
+
+a=calculating()+29
+print("WOW: " + str(a))
+'''
+
+'''
+def calculating2(x,y):
+    print("Your first number: " + str(x) + "\nYour second number: " + str(y))
+    sum=x**y
+    print("\nYour new number is: " + str(sum))
+    return sum
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+idan=calculating2(a,b) + 29
+print("WOW: " + str(idan))
+'''
+
+'''
+def add_ip(ip1,ip2,ip3):
+    list=[]
+    list.append(ip1)
+    list.append(ip2)
+    list.append(ip3)
+    return list
+
+ip_list=[]
+ip_n1=input("Enter first IP: ")
+ip_n2=input("Enter 2nd IP: ")
+ip_n3=input("Enter 3rd IP: ")
+ip_list=add_ip(ip_n1,ip_n2,ip_n3)
+ip_list=add_ip(ip_n1,ip_n2,ip_n3)
+print(ip_list)
+'''
