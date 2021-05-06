f66ed3f8e538e53b05ca37a9e80f235e58ef5db1
moshegplay <moshegplay@gmail.com>
2021-05-06 18:06:37 +0300

    Create File_Handling2.py

---
+++ lessons_2/File_Handling2.py
@@ -0,0 +1,11 @@
+
+
+
+filename = "C:/Users/moshe/PycharmProjects/hello.txt"
+file =open(filename,"r")
+my_string=file.read()
+file.close()
+print(my_string)
+my_list=my_string.split(",")
+print(type(my_list))
+print(my_list)
