1127a5dffb857acc66494a3932ee47262f11e15f
moshegplay <moshegplay@gmail.com>
2021-05-08 20:11:01 +0300

    Create Modules_main.py

---
+++ lessons_2/Modules_main.py
@@ -0,0 +1,12 @@
+from random import randint
+from time import sleep
+from lessons_2.Modules_def import menu,calculating,dogs_age
+menu()
+sleep(3)
+print("wow!\n")
+calculating(3,6)
+dogs_age(int(input("Enter dog's age: ")))
+
+
+
+
