f81b0fb13515a527d60e8467cb9b59235fc4bc43
moshegplay <moshegplay@gmail.com>
2021-05-10 23:40:01 +0300

    Update Paramiko.py

--- lessons_2/Paramiko.py
+++ lessons_2/Paramiko.py
@@ -1,3 +1,19 @@
 import paramiko
-import sys
 
+nbytes = 4096
+hostname = '192.168.1.31'
+port = 22
+username = 'idan'
+password = 'rootroot'
+command = 'mkdir paramiko_ssh'
+
+client = paramiko.Transport((hostname, port))
+client.connect(username=username, password=password)
+
+stdout_data = []
+stderr_data = []
+session = client.open_channel(kind='session')
+session.exec_command(command)
+
+session.close()
+client.close()
